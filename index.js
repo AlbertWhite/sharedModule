e567663a128fdc7d26817616ddb2e3c3b66eb20c
AlbertWhite <xyb1114@gmail.com>
2019-01-18 15:01:25 +0100

    init

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const a = require('./constants/a')
 const b = require('./constants/b')
 const helper1 = require('./helpers/helper1')
 const helper2 = require('./helpers/helper2')
-const css1 = require('./stylesheets/a.css')
-const css2 = require('./stylesheets/b.css')
 
 exports.constants = {
   a: a,
@@ -13,9 +11,4 @@ exports.constants = {
 exports.helpers = {
   helper1: helper1,
   helper2: helper2
-}
-
-exports.stylesheets = {
-  css1: css1,
-  css2: css2
 }
